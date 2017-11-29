143e6644eeb65abe6898b8091c7ec8b0313befe9
John Duggan <JLDuggan87@gmail.com>
2017-11-29 18:32:03 -0500

    set up article reviews

--- Gemfile
+++ Gemfile
@@ -1,8 +1,9 @@
 source 'https://rubygems.org/'
 
-gem 'rails', '~> 5.0.0'
+gem 'rails', '~> 5.1.4'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.0'
+gem 'bootstrap-sass', '~> 3.3.7'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
@@ -14,7 +15,6 @@ end
 
 group :development, :test do
   gem 'capybara'
-  gem 'factory_girl_rails'
   gem 'launchy', require: false
   gem 'pry-rails'
   gem 'rspec-rails', '~> 3.5'
---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,58 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update_attributes(article_params)
+      flash[:success] = "Article was successfully updated!"
+      redirect_to article_path(@article)
+    else
+      flash[:alert]
+      render 'new'
+    end
+  end
+
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash[:success] = "Article was succesfully created!"
+      redirect_to article_path(@article)
+    else
+      flash[:alert]
+      render 'new'
+    end
+
+  def destroy
+    @article.destroy
+    flash[:danger] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+
+  end
+
+  private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
+end
