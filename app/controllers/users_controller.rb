b45691602a4ad63a33e2ce06f9b8ed9662f9b437
John Duggan <JLDuggan87@gmail.com>
2017-12-02 15:48:50 -0500

    Add users index page and update show article page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -25,10 +33,6 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
-  end
-
-  def show
-    @user = User.find(params[:id])
   end
 
   private
