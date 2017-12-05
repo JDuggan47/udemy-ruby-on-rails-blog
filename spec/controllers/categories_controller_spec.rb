eb230e42401ba68bb9824e241cea9ef25d4fe50e
John Duggan <JLDuggan87@gmail.com>
2017-12-04 23:06:22 -0500

    create category table and controller tests

---
+++ spec/controllers/categories_controller_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe CategoriesController, type: :controller do
+  let!(:category)  { FactoryBot.create(:category, name: "movies") }
+
+  describe "GET#index" do
+    it "has a 200 status code" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'GET#new' do
+    it "shows the page where you can create new category" do
+      get :new
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'GET#show' do
+    it 'shows the show page of the requested category' do
+      get :show, params: { id: category.id }
+      expect(response.status).to eq(200)
+    end
+  end
+
+end
--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-subject { FactoryBot.build(:user) }
+  subject { FactoryBot.build(:user) }
 
-it { should validate_presence_of(:username) }
-it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:email) }
 
-it { should validate_uniqueness_of(:username) }
-it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
 
-it { should validate_length_of(:username).is_at_least(3).is_at_most(25) }
+  it { should validate_length_of(:username).is_at_least(3).is_at_most(25) }
 
-it { should have_many(:articles) }
+  it { should have_many(:articles) }
 end
